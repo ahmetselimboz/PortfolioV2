7ce941fcc6c25211449e3e494547202a89378d96
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-04-29 19:58:01 +0300

    Caprover-test

--- routes/blogs.js
+++ routes/blogs.js
@@ -17,7 +17,7 @@ router.get("/:id", async function (req, res, next) {
   try {
     const result = await Blogs.findById(req.params.id);
 
-    var data = await Blog.find({ _id: { $ne: req.params.id } }).limit(6);
+    var data = await Blogs.find({ _id: { $ne: req.params.id } }).limit(6).select("mainImg tags title desc");
 
     res.json(Response.successResponse({result,data}));
   } catch (error) {
