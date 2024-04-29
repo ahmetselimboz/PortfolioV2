63ed9be06d56cc8770459926e5a192c168931821
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-04-29 19:33:04 +0300

    blog-detail bug fixed

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,26 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Blogs = require("../db/models/Blogs")
+const Blogs = require("../db/models/Blogs");
 const Response = require("../lib/Response");
 
-router.get('/', async function(req, res, next) {
-    try {
-        const result = await Blogs.find();
-        res.json(Response.successResponse(result));
-      } catch (error) {
-        let errorResponse = Response.errorResponse(error);
-        res.status(errorResponse.code).json(Response.errorResponse(error));
-      }
+router.get("/", async function (req, res, next) {
+  try {
+    const result = await Blogs.find();
+    res.json(Response.successResponse(result));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(Response.errorResponse(error));
+  }
 });
 
-router.get('/:id', async function(req, res, next) {
-    try {
-        const result = await Blogs.findById(req.params.id)
-        res.json(Response.successResponse(result));
-      } catch (error) {
-        let errorResponse = Response.errorResponse(error);
-        res.status(errorResponse.code).json(Response.errorResponse(error));
-      }
+router.get("/:id", async function (req, res, next) {
+  try {
+    const result = await Blogs.findById(req.params.id);
+
+    var data = await Blog.find({ _id: { $ne: req.params.id } }).limit(6);
+
+    res.json(Response.successResponse({result,data}));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(Response.errorResponse(error));
+  }
 });
 
 module.exports = router;
