77a903e538870afc5292a0d3db2438c49438cf38
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-05-06 19:42:16 +0300

    00

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(customMorganLogger);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
